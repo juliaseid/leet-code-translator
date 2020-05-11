0495e6d8d4874560fd1d894528bf4b4860511882
Tyler Bates <Fondlytyler@gmail.com>
2020-05-11 11:59:21 -0700

    initial commit with framework.

---
+++ LeetSpeak.Tests/ModelTests/TranslatorTests.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Translator;
+
+namespace Translator.TestTools
+{
+  [TestClass]
+  public class TranslatorTests
+  {
+    [TestMethod]
+    // public void Translator_
+  }
+}
+
+
+// 
---
+++ LeetSpeak/Models/Translator.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace Translator
+{
+  public class LeetSpeakTranslator
+  {
+    public void Translate()
+    {
+      
+    }
+  }
+
+}
