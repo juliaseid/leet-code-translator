062acd7ee56f98a1edb41d3fe8799d2080b89965
Tyler Bates <Fondlytyler@gmail.com>
2020-05-11 13:48:17 -0700

    successfully pass first test.

--- LeetSpeak.Tests/ModelTests/TranslatorTests.cs
+++ LeetSpeak.Tests/ModelTests/TranslatorTests.cs
@@ -8,9 +8,17 @@ namespace Translator.TestTools
   public class TranslatorTests
   {
     [TestMethod]
-    // public void Translator_
+    public void Translator_LetterEReplacedWith_3()
+    {
+      LeetSpeakTranslator testOne = new LeetSpeakTranslator();
+      Assert.AreEqual("3", testOne.Translate("e"));
+    }
   }
 }
 
 
-// 
+// "e" shoud be replaced with "3"
+// "o" should be replaced with "0"
+// capital "I" should be replaced with "1"
+// "t" should be replaced with "7"
+// "s" should be replaced with "z" unless first letter of word
